188115a60f951414e364ebde9ca300ae3b0c2af4
leunbin <155026969+leunbin@users.noreply.github.com>
2024-09-16 23:34:19 +0900

    fix. findMemoByDuedate

--- src/data-access/memoDAO.js
+++ src/data-access/memoDAO.js
@@ -30,22 +30,17 @@ class memoDAO {
     }
   }
 
-  async findByDueDate(date) {
-    const localDate = new Date(date);
-    const offset = 9 * 60; // KST는 UTC+9, 즉 540분
+  async findByDueDate() {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    
+    const threeDaysLater = new Date(today);
+    threeDaysLater.setDate(today.getDate() + 3);
   
-    // KST 시간으로 시작과 끝 날짜 생성
-    const startOfDayLocal = new Date(localDate);
-    startOfDayLocal.setHours(0, 0, 0, 0);
-    
-    const endOfDayLocal = new Date(localDate);
-    endOfDayLocal.setHours(23, 59, 59, 999);
-    
-    // KST에서 UTC로 변환
-    const startOfDayUTC = new Date(startOfDayLocal.getTime() - offset * 60 * 1000);
-    const endOfDayUTC = new Date(endOfDayLocal.getTime() - offset * 60 * 1000);
-    
-    // UTC 시간대로 쿼리
+    const offset = 9 * 60;
+    const startOfDayUTC = new Date(today.getTime() - offset * 60 * 1000);
+    const endOfDayUTC = new Date(threeDaysLater.getTime() - offset * 60 * 1000);
+
     const notes = await Note.find({
       dueDate: { $gte: startOfDayUTC, $lt: endOfDayUTC }
     }).lean();
@@ -58,6 +53,7 @@ class memoDAO {
     
     return allMemoes.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate));
   }
+  
   
 
   async updateMemo(id, updateData) {
